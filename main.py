668b53564744fcaef8ed10aa682ab1f57e39fbe2
Grayson Welch <gwelch1002778@gmail.com>
2022-06-07 21:06:46 +0000

    Improved database setup and added create user page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
@@ -14,13 +14,23 @@ db = SQLAlchemy(app)
 class users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
     email = db.Column(db.String(100))
-    rank = db.Column(db.String(2))
+    rank = db.Column(db.String(1))
+    gender = db.Column(db.String(1))
+    bio = db.Column(db.String(500))
+    team = db.Column(db.String(25))
 
-    def __init__(self, name, email, rank):
+    def __init__(self, name, username, password, email, rank, gender, bio, team):
         self.name = name
+        self.username = username
+        self.password = password
         self.email = email
         self.rank = rank
+        self.gender = gender
+        self.bio = bio
+        self.team = team
 
 @app.route("/")
 def index():
@@ -32,6 +42,12 @@ def login():
     if request.method == "POST":
         return "<p>Working.</p>"
     return render_template("login.html")
+
+@app.route("/create_account", methods=['POST', 'GET'])
+def create_account():
+    if request.method == "POST":
+        pass
+    return render_template('create_account.html')
 
 db.create_all()
 app.run('0.0.0.0', port=8000, debug=True)
