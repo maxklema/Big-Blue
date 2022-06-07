2805712901f57d7ae36805b796a62f4688ffa0d6
Grayson Welch <gwelch1002778@gmail.com>
2022-06-07 01:53:43 +0000

    Configured the database

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
+
+app.secret_key = "max"
+app.permanent_session_lifetime = timedelta(minutes=60)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    rank = db.Column(db.String(2))
+
+    def __init__(self, name, email, rank):
+        self.name = name
+        self.email = email
+        self.rank = rank
 
 app = Flask(__name__)
 
@@ -12,4 +32,5 @@ def login():
         return "<p>Working.</p>"
     return render_template("login.html")
 
+db.create_all()
 app.run('0.0.0.0', port=8000)
