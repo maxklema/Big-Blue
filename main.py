2a1abee55688ebafc20ea4432caa219ad4df73dc
Grayson Welch <gwelch1002778@gmail.com>
2022-06-07 01:36:06 +0000

    I added a basic login page.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return "<p>Working.</p>"
+    return render_template("login.html")
 
 app.run('0.0.0.0', port=8000)
