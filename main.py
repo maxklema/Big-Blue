6e488ab4d09483dcc33a6da537764f1a85731828
Grayson Welch <gwelch1002778@gmail.com>
2022-06-08 01:21:46 +0000

    Creating account works and login to that account!

--- main.py
+++ main.py
@@ -40,13 +40,32 @@ def index():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        return "<p>Working.</p>"
+
+        username_input = request.form['username']
+        password_input = request.form['password']
+        found_user = users.query.filter_by(username=username_input).first()
+        
+        if found_user and found_user.username == username_input and found_user.password == password_input:
+            return "<p>Working.</p>"
+        else:
+            return "<p>Sorry. That is incorect.</p>"
+
     return render_template("login.html")
 
 @app.route("/create_account", methods=['POST', 'GET'])
 def create_account():
     if request.method == "POST":
-        pass
+
+        for item in request.form:
+            if request.form[item] == "":
+                return "<p>Please input all correct values.</p>"
+    
+        new_user = users(request.form['name'], request.form['username'], request.form['password'], request.form['email'], request.form['rank'], request.form['gender'], request.form['bio'], request.form['team'])
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template('create_account.html', message="Account created sucessfully!")
+
     return render_template('create_account.html')
 
 db.create_all()
